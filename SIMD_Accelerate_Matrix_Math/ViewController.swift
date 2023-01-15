1f7ccd1b8b9e92148326d1be02cd4328480b5bff
Eric Kampman <erickampman@yahoo.com>
2023-01-15 10:31:09 -0800

    Initial Commit

---
+++ SIMD_Accelerate_Matrix_Math/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SIMD_Accelerate_Matrix_Math
+//
+//  Created by Eric Kampman on 1/15/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
