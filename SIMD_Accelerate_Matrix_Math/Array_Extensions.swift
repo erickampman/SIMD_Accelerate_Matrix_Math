bc43ae505b55d5bd8a87871bd1589bde827e9410
Eric Kampman <erickampman@yahoo.com>
2023-01-15 14:54:13 -0800

    get3x3FromSquareArray

--- SIMD_Accelerate_Matrix_Math/Array_Extensions.swift
+++ SIMD_Accelerate_Matrix_Math/Array_Extensions.swift
@@ -36,7 +36,7 @@ extension Array {
 		}
 		
 		guard let r2c2 = self.getValueAsColumnArray(row: startRow+1, column: startColumn+1) as? Float,
-			  let r2c1 = self.getValueAsColumnArray(row: startRow, column: startColumn+1) as? Float,
+			  let r2c1 = self.getValueAsColumnArray(row: startRow+1, column: startColumn) as? Float,
 			  let r1c2 = self.getValueAsColumnArray(row: startRow, column: startColumn+1) as? Float,
 			  let r1c1 = self.getValueAsColumnArray(row: startRow, column: startColumn) as? Float
 		else {
@@ -48,5 +48,31 @@ extension Array {
 
 		return simd_float2x2(columns: (col1, col2))
 	}
+	
+	func get3x3FromSquareArray(startRow: UInt, startColumn: UInt) -> simd_float3x3? {
+		if (!isUIntSquare(UInt(count))) {
+			return nil
+		}
 
+		guard let r3c3 = self.getValueAsColumnArray(row: startRow+2, column: startColumn+2) as? Float,
+			  let r3c2 = self.getValueAsColumnArray(row: startRow+2, column: startColumn+1) as? Float,
+			  let r3c1 = self.getValueAsColumnArray(row: startRow+2, column: startColumn) as? Float,
+			  
+			  let r2c3 = self.getValueAsColumnArray(row: startRow+1, column: startColumn+2) as? Float,
+			  let r2c2 = self.getValueAsColumnArray(row: startRow+1, column: startColumn+1) as? Float,
+			  let r2c1 = self.getValueAsColumnArray(row: startRow+1, column: startColumn) as? Float,
+			  
+			  let r1c1 = self.getValueAsColumnArray(row: startRow, column: startColumn+2) as? Float,
+			  let r1c2 = self.getValueAsColumnArray(row: startRow, column: startColumn+1) as? Float,
+			  let r1c3 = self.getValueAsColumnArray(row: startRow, column: startColumn) as? Float
+		else {
+			return nil
+		}
+
+		let col1 = SIMD3<Float>(r1c1,r2c1,r3c1)
+		let col2 = SIMD3<Float>(r1c2,r2c2,r3c2)
+		let col3 = SIMD3<Float>(r1c3,r2c3,r3c3)
+
+		return simd_float3x3(columns: (col1, col2, col3))
+	}
 }
